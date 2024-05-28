22da70e7983bd317158fb46d72e8d5ee24d9496c
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-05-28 02:17:15 -0400

    adding PhotoGalleryViewModel

--- app/src/main/java/com/yash/android/bnr/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/yash/android/bnr/photogallery/PhotoGalleryFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.yash.android.bnr.photogallery.databinding.FragmentPhotoGalleryBinding
@@ -18,6 +19,7 @@ class PhotoGalleryFragment : Fragment() {
         get() = checkNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
         }
+    private val photoGalleryViewModel: PhotoGalleryViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,11 +34,8 @@ class PhotoGalleryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewLifecycleOwner.lifecycleScope.launch {
-            try {
-                val response = PhotoRepository().fetchPhotos()
-                Log.d(TAG, "Response received: $response")
-            } catch (ex: Exception) {
-                Log.e(TAG, "Failed to fetch gallery items", ex)
+            photoGalleryViewModel.galleryItems.collect { items ->
+                Log.d(TAG, "Response received: $items")
             }
         }
     }
