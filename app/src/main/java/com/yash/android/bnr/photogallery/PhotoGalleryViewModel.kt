761507b96f6017ad18d7a953ae4eb9c3a48dcb85
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-05-28 22:13:20 -0400

    adding search functionality for flickr

--- app/src/main/java/com/yash/android/bnr/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/yash/android/bnr/photogallery/PhotoGalleryViewModel.kt
@@ -18,8 +18,7 @@ class PhotoGalleryViewModel : ViewModel() {
     init {
         viewModelScope.launch {
             try {
-                val items = photoRepository.fetchPhotos()
-                Log.d(TAG, "Items received: $items")
+                val items = photoRepository.searchPhotos("bicycle")
                 _galleryItems.value = items
             } catch (ex: Exception) {
                 Log.e(TAG, "Failed to fetch gallery items", ex)
