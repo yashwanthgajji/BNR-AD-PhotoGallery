14ba51695d2dcbd911c4e25a7f524fb8fde60cdc
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-05-29 19:04:33 -0400

    storing the last photo id and checking with new batch

--- app/src/main/java/com/yash/android/bnr/photogallery/PollWorker.kt
+++ app/src/main/java/com/yash/android/bnr/photogallery/PollWorker.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import kotlinx.coroutines.flow.first
 
 private const val TAG = "PollWorker"
 class PollWorker(
@@ -11,7 +12,31 @@ class PollWorker(
     workerParameters: WorkerParameters
 ) : CoroutineWorker(context, workerParameters) {
     override suspend fun doWork(): Result {
-        Log.d(TAG, "Work request triggered")
-        return Result.success()
+        val preferencesRepository = PreferencesRepository.getInstance()
+        val photoRepository = PhotoRepository()
+
+        val query = preferencesRepository.storedQuery.first()
+        val lastId = preferencesRepository.lastResultId.first()
+        if (query.isEmpty()) {
+            Log.i(TAG, "No saved query, finished early")
+            return Result.success()
+        }
+
+        return try {
+            val items = photoRepository.searchPhotos(query)
+            if (items.isNotEmpty()) {
+                val newResultId = items.first().id
+                if (newResultId == lastId) {
+                    Log.i(TAG, "Still have the same result: $newResultId")
+                } else {
+                    Log.i(TAG, "Got a new result: $newResultId")
+                    preferencesRepository.setLastResultId(newResultId)
+                }
+            }
+            Result.success()
+        } catch (ex: Exception) {
+            Log.e(TAG, "Background update failerd", ex)
+            Result.failure()
+        }
     }
 }
