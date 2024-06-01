331acc61c2c59ff53381a3b1a150f5a3226b6c92
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-06-01 19:31:41 -0400

    displaying uri in webview client

--- app/src/main/java/com/yash/android/bnr/photogallery/PhotoPageFragment.kt
+++ app/src/main/java/com/yash/android/bnr/photogallery/PhotoPageFragment.kt
@@ -1,20 +1,35 @@
 package com.yash.android.bnr.photogallery
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.yash.android.bnr.photogallery.databinding.FragmentPhotoPageBinding
 
 class PhotoPageFragment: Fragment() {
 
+    private val args: PhotoPageFragmentArgs by navArgs()
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentPhotoPageBinding.inflate(inflater, container, false)
+
+        binding.apply {
+            photoWebView.apply {
+                settings.javaScriptEnabled = true
+                webViewClient = WebViewClient()
+                loadUrl(args.photoPageUri.toString())
+            }
+        }
+
         return binding.root
     }
 }
